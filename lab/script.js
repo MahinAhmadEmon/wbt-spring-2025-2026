55290e7dff465b11a6b0f1f8c0798eb96c69edd5
Mahin Ahmad Emon <mahinahmad911@gmail.com>
2026-03-03 10:37:47 +0600

    js basic

---
+++ lab/script.js
@@ -0,0 +1,31 @@
+
+
+const a = 10;
+const b = 5;
+
+console.log("value before swap a = ", a);
+console.log("value before swap b = ", b);
+
+console.log("value after swap a = ", a - b);
+console.log("value after swap b = ", b + b);
+
+
+
+
+function square(n) {
+    return n * n;
+}
+
+console.log("the square value is ", square(8));
+
+
+const number = [1, 2, 3, 4, 5];
+
+function large() {
+    num
+}
+
+
+
+
+
